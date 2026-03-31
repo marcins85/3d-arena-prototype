28952758531c93b48ca6d8a016cec1ba3fd95706
Marcin Stachowicz <stassimon@gmail.com>
2026-03-31 15:03:48 +0200

    fix: adding flag when jump not started but setting to idle animationa state

--- Assets/Scripts/Features/Player/Animation/Locomotion/States/JumpState.cs
+++ Assets/Scripts/Features/Player/Animation/Locomotion/States/JumpState.cs
@@ -1,12 +1,16 @@
+using NUnit.Framework;
+
 public class JumpState : IState
 {
     private LocomotionContext _ctx;
     private LocomotionStateMachine _sm;
+    private bool _hasLeftGround;
 
     public JumpState(LocomotionContext ctx, LocomotionStateMachine sm)
     {
         _ctx = ctx;
         _sm = sm;
+        _hasLeftGround = false;
     }
 
     public void Enter()
@@ -14,11 +18,18 @@ public class JumpState : IState
         _ctx.Animator.SetTrigger("Jump");
     }
 
-    public void Exit() { }
+    public void Exit() 
+    {
+    }
 
     public void Update()
     {
-        if (_ctx.IsGrounded)
+        if (!_hasLeftGround && !_ctx.IsGrounded)
+        {
+            _hasLeftGround = true;
+        }
+
+        if (_hasLeftGround && _ctx.IsGrounded)
         {
             _sm.SetState(new IdleState(_ctx, _sm));
         }
