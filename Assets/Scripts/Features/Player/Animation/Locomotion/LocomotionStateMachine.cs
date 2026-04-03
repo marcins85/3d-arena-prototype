44e20f9707aeb131a50bd20a34ec4e435e3b9ccd
Marcin Stachowicz <stassimon@gmail.com>
2026-04-03 17:42:38 +0200

    refactor: delete temporary jump start state

--- Assets/Scripts/Features/Player/Animation/Locomotion/LocomotionStateMachine.cs
+++ Assets/Scripts/Features/Player/Animation/Locomotion/LocomotionStateMachine.cs
@@ -3,13 +3,11 @@ public class LocomotionStateMachine : StateMachine
     public IState Idle;
     public IState Move;
     public IState Jump;
-    public IState JumpStart;
 
     public LocomotionStateMachine(LocomotionContext ctx, IJump jump)
     {
         Idle = new IdleState(ctx, this);
         Move = new MoveState(ctx, this);
         Jump = new JumpState(ctx, this, jump);
-        JumpStart = new JumpStartState(ctx, this, jump);
     }
 }
