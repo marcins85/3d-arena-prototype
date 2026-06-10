ee34a91eb54f2a21cde8fbb17dc60b0d448285fb
Marcin Stachowicz <stassimon@gmail.com>
2026-06-10 17:02:24 +0200

    feat: adding simplel controller for enemy and adding interfece for ai

--- Assets/Scripts/Features/Enemy/Movement/EnemyMovement.cs
+++ Assets/Scripts/Features/Enemy/Movement/EnemyMovement.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 
 public class EnemyMovement : IMovement
 {
+    private CharacterController _characterController;
+    private EnemyConfigSO _config;
+    private LayerMask _groundMask;
     public float CurrentVerticalVelocity => throw new NotImplementedException();
-
     public MovementState State { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
     public bool CanMove => throw new NotImplementedException();
+
+    public EnemyMovement(CharacterController characterController, EnemyConfigSO config, LayerMask groundMask)
+    {
+        _characterController = characterController;
+        _config = config;
+        _groundMask = groundMask;
+    }
 
     public void HandleMovement(float verticalVelocity)
     {
